b2268e34839d1788c8a36e8bc25c622d52d4a1e0
jeremyhsimons <jeremysimons15@gmail.com>
2023-09-01 19:32:57 +0000

    Create profile serializer and listview

---
+++ profiles/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Profile
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    profile_owner = serializers.ReadOnlyField(source="profile_owner.username")
+
+    class Meta:
+        model = Profile
+        fields = [
+            "id", "profile_owner", "created_at",
+            "updated_at", "name", "bio", "image",
+            "status",
+        ]
