4b1d6a77e42041b9ce77af76b841876a5bddf018
jeremyhsimons <jeremysimons15@gmail.com>
2023-09-01 07:58:38 +0000

    Add profile app and model.

---
+++ profiles/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+
+    CHOICES = (
+        ('LFH', 'Looking for help'),
+        ('SME', 'Subject matter expert'),
+        ('JB', 'Just browsing'),
+        ('AME', 'Ask me anything'),
+        ('NA', 'Not active')
+    )
+
+    profile_owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=255, blank=True)
+    bio = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default="../default_profile_uk3zc1"
+    )
+    status = models.CharField(max_length=300, choices=CHOICES)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.profile_owner}'s profile"
