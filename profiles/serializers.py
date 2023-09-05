83503942fbfa34777308d6066679c06ef7dcec63
jeremyhsimons <jeremysimons15@gmail.com>
2023-09-05 14:21:22 +0000

    Wire up custom permissions and authentication.

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,11 +4,16 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     profile_owner = serializers.ReadOnlyField(source="profile_owner.username")
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.profile_owner
 
     class Meta:
         model = Profile
         fields = [
             "id", "profile_owner", "created_at",
             "updated_at", "name", "bio", "image",
-            "status",
+            "status", "is_owner",
         ]
