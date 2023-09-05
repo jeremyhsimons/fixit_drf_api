38de4826327d77b794837f4e7198015023292993
jeremyhsimons <jeremysimons15@gmail.com>
2023-09-05 16:25:38 +0000

    Add posts app to project.

--- profiles/tests.py
+++ profiles/tests.py
@@ -20,7 +20,7 @@ class ProfileListViewTests(APITestCase):
         tester = User.objects.get(username='test')
         response = self.client.get('/profiles/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-    
+
     def test_can_edit_own_profile(self):
         """
         Test user can update their own profile. 
