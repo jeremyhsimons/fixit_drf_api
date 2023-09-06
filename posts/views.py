e7f7e0530839226427825bf0bf9219a104349958
jeremyhsimons <jeremysimons15@gmail.com>
2023-09-06 08:39:28 +0000

    add get request for the post detail view.

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.http import Http404
 
 from .models import Post
 from .serializers import PostSerializer
@@ -33,4 +34,19 @@ class PostList(APIView):
 
 class PostDetail(APIView):
     serializer_class = PostSerializer
-    permission_classes = [IsOwnerOrReadOnly]
+    permission_classes = [IsPostOwnerOrReadOnly]
+
+    def get_object(self, pk):
+        try:
+            post = Post.objects.get(pk=pk)
+            self.check_object_permissions(self.request, post)
+            return post
+        except Post.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(
+            post, context={'request': request}
+        )
+        return Response(serializer.data)
