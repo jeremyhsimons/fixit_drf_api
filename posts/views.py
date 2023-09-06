7c0709cf6f7b74f6c85a27893b75383fc1c04d56
jeremyhsimons <jeremysimons15@gmail.com>
2023-09-06 08:54:47 +0000

    Add a post delete method in the postdetail view

--- posts/views.py
+++ posts/views.py
@@ -5,6 +5,7 @@ from django.http import Http404
 
 from .models import Post
 from .serializers import PostSerializer
+from fixit_drf_api.permissions import IsPostOwnerOrReadOnly
 
 
 class PostList(APIView):
@@ -50,3 +51,20 @@ class PostDetail(APIView):
             post, context={'request': request}
         )
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(
+            post, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
