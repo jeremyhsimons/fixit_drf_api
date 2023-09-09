1c71706077a11fd1e601ea8b2a453358331aa2b6
jeremyhsimons <jeremysimons15@gmail.com>
2023-09-09 16:41:37 +0000

    add model for bookmark and migrate to db

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
 
-# Create your models here.
+
+class Bookmark(models.Model):
+    """
+    A class for users bookmarks to save a post
+    for later viewing. It contains both the user
+    and relevant post as foreign keys.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='bookmarks'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['owner', 'post']
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s bookmark for {self.post.author}'s post"
