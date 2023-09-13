da2ef4a59cc7442a5187cd2e2f3cc28461d605f0
jeremyhsimons <jeremysimons15@gmail.com>
2023-09-13 07:28:54 +0000

    Add tests to cover retrieve and deleting bookmarks

--- bookmarks/tests.py
+++ bookmarks/tests.py
@@ -46,7 +46,7 @@ class BookmarkListViewTest(APITestCase):
 
     def test_auth_user_can_create_bookmark(self):
         """
-        Tests that a logged in user can add a bookmark 
+        Tests that a logged in user can add a bookmark
         """
         self.client.login(username='test', password="test123")
         response = self.client.post('/bookmarks/', {
@@ -54,7 +54,7 @@ class BookmarkListViewTest(APITestCase):
             'post': 1
         })
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-    
+
     def test_user_cannot_bookmark_same_post_twice(self):
         """
         Checks that the user can't duplicate a bookmark.
@@ -75,4 +75,59 @@ class BookmarkListViewTest(APITestCase):
             'owner': 1,
             'post': 1
         })
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class BookmarkDetailTest(APITestCase):
+    """
+    A test suite for bookmark detail view.
+    """
+    def setUp(self):
+        """
+        Creates test users, posts, and bookmarks
+        for this test module.
+        """
+        test1 = User.objects.create_user(username='test', password="test123")
+        test2 = User.objects.create_user(username='test2', password="test456")
+        post1 = Post.objects.create(
+            author=test1,
+            content="this is test1's post",
+            category="BC"
+        )
+        post2 = Post.objects.create(
+            author=test2,
+            content="this is test2's post",
+            category="BC"
+        )
+        Bookmark1 = Bookmark.objects.create(
+            owner=test1,
+            post=post2
+        )
+        Bookmark2 = Bookmark.objects.create(
+            owner=test2,
+            post=post1
+        )
+
+    def test_user_can_retrieve_bookmark(self):
+        """
+        Tests that a user can retrieve bookmark by ID.
+        """
+        self.client.login(username='test', password="test123")
+        response = self.client.get('/bookmarks/1/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_can_remove_bookmark(self):
+        """
+        Checks that a user can delete their own bookmarks.
+        """
+        self.client.login(username='test', password="test123")
+        response = self.client.delete('/bookmarks/1/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_user_cannot_delete_other_bookmark(self):
+        """
+        Checks that users can't delete another user's bookmark.
+        """
+        self.client.login(username='test', password="test123")
+        response = self.client.delete('/bookmarks/2/')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
