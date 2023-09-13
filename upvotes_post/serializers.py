5340bd2d07981e1d0ba4321edda0e2fc6048b63b
jeremyhsimons <jeremysimons15@gmail.com>
2023-09-13 18:57:26 +0000

    Add post upvote serializer.

---
+++ upvotes_post/serializers.py
@@ -0,0 +1,27 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from .models import PostUpvote
+
+
+class PostUpvoteSerializer(serializers.ModelSerializer):
+    """
+    Handles the data entry for post upvote instances.
+    """
+    owner = serializers.ReadOnlyField(source="owner.username")
+
+    class Meta:
+        model = Star
+        fields = [
+            "owner",
+            "post",
+            "created_at",
+            "id"
+        ]
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                "detail": "possible post_upvote duplication"
+            })
