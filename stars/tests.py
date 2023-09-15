9c1d54e0b51afd5c0a3f4c072e13c7af641c6482
jeremyhsimons <jeremysimons15@gmail.com>
2023-09-15 07:41:20 +0000

    add tests for the list and create star feature.

--- stars/tests.py
+++ stars/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Star
+from profiles.models import Profile
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class StarListTest(APITestCase):
+    """
+    A test module for the StarList View
+    """
+    def setUp(self):
+        """
+        Create user, profile, and star for test db
+        """
+        test1 = User.objects.create_user(username='test', password="test123")
+        test2 = User.objects.create_user(username='test2', password="test456")
+        star1 = Star.objects.create(owner=test1, profile=test2.profile)
+        star2 = Star.objects.create(owner=test2, profile=test1.profile)
+
+    def test_get_star_list(self):
+        """
+        Checks that a user can get a list of stars.
+        """
+        response = self.client.get('/stars/')
+        stars = Star.objects.all()
+        self.assertEqual(len(stars), 2)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_star(self):
+        """
+        Checks that an authorized user can star a profile.
+        """
+        self.client.login(username='test', password="test123")
+        response = self.client.post('/stars/', {
+            'owner': 1,
+            'profile': 1,
+        })
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_cannot_duplicate_star(self)
--- upvotes_comment/views.py
+++ upvotes_comment/views.py
@@ -16,7 +16,7 @@ class CommentUpvoteList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-    
+
 class CommentUpvoteDetail(generics.RetrieveDestroyAPIView):
     """
     A view for retrieving and deleting
