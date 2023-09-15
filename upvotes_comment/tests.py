cc2dabd2e829bac2dd333d09961cd2c87ad8e5a3
jeremyhsimons <jeremysimons15@gmail.com>
2023-09-15 20:54:01 +0000

    add tsimilar tests from star app for comment upvote list.

--- upvotes_comment/tests.py
+++ upvotes_comment/tests.py
@@ -1,3 +1,88 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import CommentUpvote
+from comments.models import Comment
+from posts.models import Post
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class CommentUpvoteListTest(APITestCase):
+    """
+    A test module for the CommentUpvoteList view.
+    """
+    def setUp(self):
+        """
+        Create user, post, comment, upvote for test db.
+        """
+        test1 = User.objects.create_user(username='test', password="test123")
+        test2 = User.objects.create_user(username='test2', password="test456")
+        post1 = Post.objects.create(
+            author=test1,
+            content="this is test1's post",
+            category="BC"
+        )
+        post2 = Post.objects.create(
+            author=test2,
+            content="this is test2's post",
+            category="BC"
+        )
+        comment1 = Comment.objects.create(
+            author=test1,
+            post=post2,
+            content="This is test comment 1 on post 2"
+        )
+        comment2 = Comment.objects.create(
+            author=test2,
+            post=post1,
+            content="This is test comment 2 on post 1"
+        )
+        upvote1 = CommentUpvote.objects.create(owner=test1, comment=comment2)
+        upvote2 = CommentUpvote.objects.create(owner=test2, comment=comment1)
+
+    def test_get_upvote_list(self):
+        """
+        Checks that a user can get a list of comment upvotes.
+        """
+        response = self.client.get('/comment-upvotes/')
+        upvotes = CommentUpvote.objects.all()
+        self.assertEqual(len(upvotes), 2)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_upvote(self):
+        """
+        Checks that an authorized user can upvote a comment.
+        """
+        self.client.login(username='test', password="test123")
+        response = self.client.post('/comment-upvotes/', {
+            'owner': 1,
+            'comment': 1,
+        })
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_cannot_duplicate_upvote(self):
+        """
+        Checks that users can't upvote the same comment twice.
+        """
+        self.client.login(username='test', password="test123")
+        response = self.client.post('/comment-upvotes/', {
+            'owner': 1,
+            'comment': 2,
+        })
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_unauth_user_cant_create_upvote(self):
+        """
+        Checks that users can't upvote if they're logged out.
+        """
+        response = self.client.post('/comment-upvotes/', {
+            'owner': 1,
+            'comment': 1,
+        })
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class UpvoteCommentDetailTest(APITestCase):
+    """
+    A module of tests for upvote comment detail view.
+    """
