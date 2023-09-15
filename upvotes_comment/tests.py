b949041166f8cf746fc1e111684d8df062863fcb
jeremyhsimons <jeremysimons15@gmail.com>
2023-09-15 20:59:30 +0000

    Add similar tests for upvote comment detail view.

--- upvotes_comment/tests.py
+++ upvotes_comment/tests.py
@@ -86,3 +86,62 @@ class UpvoteCommentDetailTest(APITestCase):
     """
     A module of tests for upvote comment detail view.
     """
+    def setUp(self):
+        """
+        Create user, post, comment, upvote for test db.
+        """
+        test1 = User.objects.create_user(username='test', password="test123")
+        test2 = User.objects.create_user(username='test2', password="test456")
+        post1 = Post.objects.create(
+            author=test1,
+            content="this is test1's post",
+            category="BC"
+        )
+        post2 = Post.objects.create(
+            author=test2,
+            content="this is test2's post",
+            category="BC"
+        )
+        comment1 = Comment.objects.create(
+            author=test1,
+            post=post2,
+            content="This is test comment 1 on post 2"
+        )
+        comment2 = Comment.objects.create(
+            author=test2,
+            post=post1,
+            content="This is test comment 2 on post 1"
+        )
+        upvote1 = CommentUpvote.objects.create(owner=test1, comment=comment2)
+        upvote2 = CommentUpvote.objects.create(owner=test2, comment=comment1)
+
+    def test_can_retrieve_upvote(self):
+        """
+        Checks that a user can retrive upvote
+        """
+        self.client.login(username='test', password="test123")
+        response = self.client.get('/comment-upvotes/1/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_can_delete_upvote(self):
+        """
+        Checks that a user can delete an upvote they own
+        """
+        self.client.login(username='test', password="test123")
+        response = self.client.delete('/comment-upvotes/1/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_unauth_user_cannot_delete_upvote(self):
+        """
+        Checks that upvotes can't be deleted by unuathorised users.
+        """
+        response = self.client.delete('/comment-upvotes/1/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_user_cannot_delete_others_upvote(self):
+        """
+        Checks that a user cannot delete an upvote they don't own
+        """
+        self.client.login(username='test', password="test123")
+        response = self.client.delete('/comment-upvotes/2/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
