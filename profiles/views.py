0f937c67651ab7ba967c3bc803fa81700f3ecd5b
jeremyhsimons <jeremysimons15@gmail.com>
2023-09-19 16:52:57 +0000

    tidy up profiles view.py file

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,8 @@
-from rest_framework.views import APIView
-from rest_framework import status
-from rest_framework.response import Response
-
 from django.db.models import Count
 from rest_framework import generics, filters
+
 from .models import Profile
 from .serializers import ProfileSerializer
-from django.http import Http404
 from fixit_drf_api.permissions import IsOwnerOrReadOnly
 
 
@@ -30,40 +26,13 @@ class ProfileList(generics.ListAPIView):
     ]
 
 
-# class ProfileDetail(APIView):
-#     serializer_class = ProfileSerializer
-#     permission_classes = [IsOwnerOrReadOnly]
-
-#     def get_object(self, pk):
-#         try:
-#             profile = Profile.objects.get(pk=pk)
-#             self.check_object_permissions(self.request, profile)
-#             return profile
-#         except Profile.DoesNotExist:
-#             raise Http404
-
-#     def get(self, request, pk):
-#         profile = self.get_object(pk)
-#         serializer = ProfileSerializer(
-#             profile, context={'request': request}
-#         )
-#         return Response(serializer.data)
-
-#     def put(self, request, pk):
-#         profile = self.get_object(pk)
-#         serializer = ProfileSerializer(
-#             profile, data=request.data, context={'request': request}
-#         )
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
     Retrieve, update profiles for authenticated users.
     """
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count("profile_owner__post", distinct=True),
+        stars_count=Count('stars', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
